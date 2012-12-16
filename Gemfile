f862c713bbc9d7b95eba038d44b0b1c66db22eea
zh99998 <zh99998@gmail.com>
2012-12-16 09:10:39 +0800

    websocket init

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source :rubygems
-gem 'eventmachine'
-gem 'em-http-request'
-gem 'xmpp4r'
-gem 'daemons'
--- mycard-server.rb
+++
@@ -1,4 +0,0 @@
-  require 'daemons'
-  
-    Daemons.run('main.rb', dir: File.dirname(__FILE__), monitor: true)
-  
